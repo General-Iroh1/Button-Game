07c821d7ebc45b061bd8f0e834c764e5dfa9c8a5
General-Iroh1 <yaniibear5@gmail.com>
2022-12-12 10:44:31 +0000

    The retry button now moves when clicked

--- Buttons.py
+++ Buttons.py
@@ -5,26 +5,32 @@ n = 0
 x = 0
 s = 0
 c = 0
-l = 0
+l = 1
+y = 1
 score = 0
-e = random.randint(0,800)
-f = random.randint(1,1560)
+e = random.randint(0,760)
+f = random.randint(1,1520)
 print (e)
 print (f)
 fe =str(f)
 ef =str(e)
 def GameOver():
+    global l
+    l = 1
     if s>= 1:
         screen3.destroy()
     sscore = str(score)
     global screen2
     screen2=Toplevel(screen)
-    screen2.geometry("1560x800")
+    screen2.attributes('-fullscreen', True)
     screen2.title("Game Over!")
     Label(screen2,text="You got a score of "+sscore).pack()
     Label(screen2,text="Game Over! Better luck next time!").pack()
-    Button(screen2,text="Quit & Retry", command=Retry).pack()
+    Button(screen2,text="Retry", command=Retry).pack()
+    Button(screen2,text="Quit", command=quit).pack()
 def Retry():
+    global l
+    l+=1
     global c
     c+=1
     global x
@@ -34,7 +40,7 @@ def Retry():
     s+=1
     global screen3
     screen3 = Toplevel(screen)
-    screen3.geometry("1560x800")
+    screen3.attributes('-fullscreen', True)
     screen3.title("Buttons, and more buttons!")
     Label(screen3,text = "Lets  play a game!").pack()
     Label(screen3,text = "").pack()
@@ -47,7 +53,7 @@ def Retry():
 def Buttons():
     global screen
     screen = tkinter.Tk()
-    screen.geometry("1560x800")
+    screen.attributes('-fullscreen', True)
     screen.title("Buttons, and more buttons!")
     Label(text = "Lets  play a game!").pack()
     Label(text = "").pack()
@@ -57,19 +63,24 @@ def Buttons():
     global f
     btn.place(x=f, y=e)
     Label(text = "").pack()
-    Button(text="DONT CLICK ME!",command=GameOver).pack()
+    global btn2
+    btn2 =Button(screen,text="DONT CLICK ME!", bg='blue',command=GameOver)
     screen.mainloop()
 def Score():
+    global l
     global s
+    global c
+    global score
+    if c == 1:
+        global score
+        score = 0
     if s >= 1:
         global x
         x +=1
-    global c
     if c != 1:
         x = 0
     global l
     if x == 1 and l == 1:
-        global score
         score = 0
     global n
     n+=1
@@ -83,14 +94,23 @@ def Score():
             print (score)
             global e
             global f
-            e = random.randint(0,800)
-            f = random.randint(1,1560)
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
             btn1.place(x=f, y=e)
-        score= score+1
-        print (score)
-        e = random.randint(0,800)
-        f = random.randint(1,1560)
-        btn.place(x=f, y=e)
+        if l == 1:
+            score= score+1
+            print (score)
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn.place(x=f, y=e)
+        if y == 1:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+        if y == 2:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
     c = 0
 global all
 Buttons()
