58f23ad09e8f14368ac7bbc96bf140756eb216be
General-Iroh1 <yaniibear5@gmail.com>
2022-12-19 16:44:55 +0000

    Finished new gamemode

--- Buttons.py
+++ Buttons.py
@@ -1,6 +1,9 @@
 import tkinter
 from tkinter import *
 import random
+
+#The screen is lava!
+
 n = 0
 x = 0
 s = 0
@@ -112,5 +115,364 @@ def Score():
             k = random.randint(1,1520)
             btn2.place(x=k, y=j)
     c = 0
+
+#Timed
+
+n = 0
+x = 0
+s = 0
+c = 0
+l = 1
+y = 1
+score = 0
+e = random.randint(0,760)
+f = random.randint(1,1520)
+print (e)
+print (f)
+fe =str(f)
+ef =str(e)
+def GameOver():
+    global l
+    l = 1
+    if s>= 1:
+        screen3.destroy()
+    sscore = str(score)
+    global screen2
+    screen2=Toplevel(screen)
+    screen2.attributes('-fullscreen', True)
+    screen2.title("Game Over!")
+    Label(screen2,text="You got a score of "+sscore).pack()
+    Label(screen2,text="Game Over! Better luck next time!").pack()
+    Button(screen2,text="Retry", command=Retry).pack()
+    Button(screen2,text="Quit", command=quit).pack()
+def Retry():
+    global l
+    l+=1
+    global c
+    c+=1
+    global x
+    if x >= 1:
+        score == 0
+    global s
+    s+=1
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.attributes('-fullscreen', True)
+    screen3.title("Buttons, and more buttons!")
+    Label(screen3,text = "Lets  play a game!").pack()
+    Label(screen3,text = "").pack()
+    global btn1
+    btn1 = Button(screen3, text = 'Click me !', command = Score)
+    global e
+    global f
+    btn1.place(x=f, y=e)
+    Button(screen3,text="DONT CLICK ME!",command=GameOver).pack()
+def Buttons():
+    global screen
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(text = "Lets  play a game!").pack()
+    Label(text = "").pack()
+    global btn
+    btn = Button(screen, text = 'Click me !', command = Score)
+    global e
+    global f
+    btn.place(x=f, y=e)
+    Label(text = "").pack()
+    global btn2
+    btn2 =Button(screen,text="DONT CLICK ME!", bg='blue',command=GameOver)
+    screen.mainloop()
+def Score():
+    global l
+    global s
+    global c
+    global score
+    if c == 1:
+        global score
+        score = 0
+    if s >= 1:
+        global x
+        x +=1
+    if c != 1:
+        x = 0
+    global l
+    if x == 1 and l == 1:
+        score = 0
+    global n
+    n+=1
+    if n == 1:
+        score = 0
+        score  = score + 1
+        print(score)
+    else:
+        if l == 2:
+            score= score+1
+            print (score)
+            global e
+            global f
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn1.place(x=f, y=e)
+        if l == 1:
+            score= score+1
+            print (score)
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn.place(x=f, y=e)
+        if y == 1:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+        if y == 2:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+    c = 0
+
+#Button Madness
+
+n = 0
+x = 0
+s = 0
+c = 0
+l = 1
+y = 1
+score = 0
+e = random.randint(0,760)
+f = random.randint(1,1520)
+print (e)
+print (f)
+fe =str(f)
+ef =str(e)
+def GameOver():
+    global l
+    l = 1
+    if s>= 1:
+        screen3.destroy()
+    sscore = str(score)
+    global screen2
+    screen2=Toplevel(screen)
+    screen2.attributes('-fullscreen', True)
+    screen2.title("Game Over!")
+    Label(screen2,text="You got a score of "+sscore).pack()
+    Label(screen2,text="Game Over! Better luck next time!").pack()
+    Button(screen2,text="Retry", command=Retry).pack()
+    Button(screen2,text="Quit", command=quit).pack()
+def Retry():
+    global l
+    l+=1
+    global c
+    c+=1
+    global x
+    if x >= 1:
+        score == 0
+    global s
+    s+=1
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.attributes('-fullscreen', True)
+    screen3.title("Buttons, and more buttons!")
+    Label(screen3,text = "Lets  play a game!").pack()
+    Label(screen3,text = "").pack()
+    global btn1
+    btn1 = Button(screen3, text = 'Click me !', command = Score)
+    global e
+    global f
+    btn1.place(x=f, y=e)
+    Button(screen3,text="DONT CLICK ME!",command=GameOver).pack()
+def Buttons():
+    global screen
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(text = "Lets  play a game!").pack()
+    Label(text = "").pack()
+    global btn
+    btn = Button(screen, text = 'Click me !', command = Score)
+    global e
+    global f
+    btn.place(x=f, y=e)
+    Label(text = "").pack()
+    global btn2
+    btn2 =Button(screen,text="DONT CLICK ME!", bg='blue',command=GameOver)
+    screen.mainloop()
+def Score():
+    global l
+    global s
+    global c
+    global score
+    if c == 1:
+        global score
+        score = 0
+    if s >= 1:
+        global x
+        x +=1
+    if c != 1:
+        x = 0
+    global l
+    if x == 1 and l == 1:
+        score = 0
+    global n
+    n+=1
+    if n == 1:
+        score = 0
+        score  = score + 1
+        print(score)
+    else:
+        if l == 2:
+            score= score+1
+            print (score)
+            global e
+            global f
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn1.place(x=f, y=e)
+        if l == 1:
+            score= score+1
+            print (score)
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn.place(x=f, y=e)
+        if y == 1:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+        if y == 2:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+    c = 0
+
+#Normal
+
+n = 0
+x = 0
+s = 0
+c = 0
+l = 1
+y = 1
+score = 0
+e = random.randint(0,760)
+f = random.randint(1,1520)
+print (e)
+print (f)
+fe =str(f)
+ef =str(e)
+def GameOver1():
+    global l
+    l = 1
+    if s>= 1:
+        screen3.destroy()
+    sscore = str(score)
+    global screen2
+    screen2=Toplevel(screen)
+    screen2.attributes('-fullscreen', True)
+    screen2.title("Game Over!")
+    Label(screen2,text="You got a score of "+sscore).pack()
+    Label(screen2,text="Game Over! Better luck next time!").pack()
+    Button(screen2,text="Retry", command=Retry1).pack()
+    Button(screen2,text="Quit", command=quit).pack()
+def Retry1():
+    global l
+    l+=1
+    global c
+    c+=1
+    global x
+    if x >= 1:
+        score == 0
+    global s
+    s+=1
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.attributes('-fullscreen', True)
+    screen3.title("Buttons, and more buttons!")
+    Label(screen3,text = "Lets  play a game!").pack()
+    Label(screen3,text = "").pack()
+    global btn1
+    btn1 = Button(screen3, text = 'Click me !', command = Score1)
+    global e
+    global f
+    btn1.place(x=f, y=e)
+    Button(screen3,text="DONT CLICK ME!",command=GameOver1).pack()
+def Buttons1():
+    global screen
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(text = "Lets  play a game!").pack()
+    Label(text = "").pack()
+    global btn
+    btn = Button(screen, text = 'Click me !', command = Score1)
+    global e
+    global f
+    btn.place(x=f, y=e)
+    Label(text = "").pack()
+    global btn2
+    btn2 =Button(screen,text="DONT CLICK ME!", bg='blue',command=GameOver1)
+    screen.mainloop()
+def Score1():
+    global l
+    global s
+    global c
+    global score
+    if c == 1:
+        global score
+        score = 0
+    if s >= 1:
+        global x
+        x +=1
+    if c != 1:
+        x = 0
+    global l
+    if x == 1 and l == 1:
+        score = 0
+    global n
+    n+=1
+    if n == 1:
+        score = 0
+        score  = score + 1
+        print(score)
+    else:
+        if l == 2:
+            score= score+1
+            print (score)
+            global e
+            global f
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn1.place(x=f, y=e)
+        if l == 1:
+            score= score+1
+            print (score)
+            e = random.randint(0,760)
+            f = random.randint(1,1520)
+            btn.place(x=f, y=e)
+        if y == 1:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+        if y == 2:
+            j = random.randint(0,760)
+            k = random.randint(1,1520)
+            btn2.place(x=k, y=j)
+    c = 0
+
+def Starting_Screen():
+    global screen
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(text = "Lets  play a game!").pack()
+    Label(text = "").pack()
+    global btn
+    btn = Button(screen, text = 'Click me !', command = Score1)
+    global e
+    global f
+    btn.place(x=f, y=e)
+    Label(text = "").pack()
+    global btn2
+    btn2 =Button(screen,text="DONT CLICK ME!", bg='blue',command=GameOver1)
+    screen.mainloop()
 global all
-Buttons()
+Starting_Screen()
