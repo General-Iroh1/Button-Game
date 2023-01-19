88b93f15fe732ce234df707911d68519d584ac5b
General-Iroh1 <yaniibear5@gmail.com>
2023-01-19 16:54:49 +0000

    testing new countdown for game.

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
-import time
 import tkinter
+from tkinter import *
+import random
+import time
+from tkinter import messagebox
 def testscreen():
     screen = tkinter.Tk()
     screen.attributes('-fullscreen', True)
     screen.title("Buttons, and more buttons!")
-    
-# define the countdown func.
+    Label(screen,text="hi").pack()
 def countdown(t):
-    t = input("Enter the time in seconds: ")
     
-    while t:
+    while t < 6:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
-      
+    testscreen()
     print('Fire in the hole!!')
   
   
@@ -24,5 +25,6 @@ t = input("Enter the time in seconds: ")
   
 # function call
 countdown(int(t))
+
 global all
 testscreen()
