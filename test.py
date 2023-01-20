228d62635529c149202d2cae4df89f377bca1b85
General-Iroh1 <yaniibear5@gmail.com>
2023-01-20 17:18:41 +0000

    testing

--- test.py
+++ test.py
@@ -3,25 +3,26 @@ from tkinter import *
 import random
 import time
 from tkinter import messagebox
-def testscreen():
-    screen = tkinter.Tk()
-    screen.attributes('-fullscreen', True)
-    screen.title("Buttons, and more buttons!")
-    Label(screen,text="hi").pack()
 def countdown(t):
     
-    while t < 6:
+    while t:
+        
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
-    testscreen()
     print('Fire in the hole!!')
   
   
 # input time in seconds
 t = input("Enter the time in seconds: ")
+def testscreen():
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(screen,text="hi").pack()
+    Button(screen,text="start timer", command=countdown).pack()
   
 # function call
 countdown(int(t))
