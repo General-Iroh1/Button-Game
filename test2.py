5c04df6114a6ab2ff4f5f82e7653a44154bf2b5e
General-Iroh1 <yaniibear5@gmail.com>
2023-01-26 17:30:49 +0000

    Testing up and down buttons

--- test2.py
+++ test2.py
@@ -1,19 +1,25 @@
 import tkinter
 from tkinter import *
-l = 5
+l=5
 p = 0
+hi =  tkinter.Tk()
+downarrow = PhotoImage(file=r"D:/Applications/Python/Button-Game/button_down.png")
+uparrow = PhotoImage(file=r"D:/PC/Pictures/Saved Pictures/button_up.png")
+hi.withdraw()
 def down():
     global l
     l-=1
-    screen.update()
+    screen.after(1, testscreen)
 def testscreen():
+    global p
+    p += 1
     global screen
-    screen = tkinter.Tk()
+    screen = Toplevel(hi)
     screen.attributes('-fullscreen', True)
     screen.title("Buttons, and more buttons!")
-    button_down =PhotoImage('button_down')
     Label(screen,text=l).pack()
-    Button(screen,image=(button_down), command=down,width=1000,height=1000, pady=100).pack()
+    Button(screen, image=downarrow,command=down,width=300,height=200, pady=10).pack()
+    Button(screen, image=uparrow,command=down,width=300,height=200, pady=10).pack()
     screen.mainloop()
 global all
 testscreen()
