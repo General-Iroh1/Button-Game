c185cec6ee23dd43d99a97e8648114cfa16783a3
General-Iroh1 <yaniibear5@gmail.com>
2023-01-30 15:46:38 +0000

    added up arrow

--- test2.py
+++ test2.py
@@ -1,10 +1,14 @@
 import tkinter
 from tkinter import *
-l=5
+l=1
 p = 0
 hi =  tkinter.Tk()
 downarrow = PhotoImage(file=r"D:/Applications/Python/Button-Game/button_down.png")
 uparrow = PhotoImage(file=r"D:/PC/Pictures/Saved Pictures/button_up.png")
+def up():
+    global l
+    l +=1
+    screen.after(1, testscreen)
 hi.withdraw()
 def down():
     global l
@@ -17,9 +21,38 @@ def testscreen():
     screen = Toplevel(hi)
     screen.attributes('-fullscreen', True)
     screen.title("Buttons, and more buttons!")
-    Label(screen,text=l).pack()
-    Button(screen, image=downarrow,command=down,width=300,height=200, pady=10).pack()
-    Button(screen, image=uparrow,command=down,width=300,height=200, pady=10).pack()
+    Label(screen,text=l,font=('Calibri', 26)).pack()
+    if l ==5:
+        Label(screen,text="5 minutes is the max you can go!",font=('Times', 26)).pack()
+    if l < 5:
+        Button(screen, image=uparrow,command=up,width=300,height=200, pady=10).pack()
+    if l == 1:
+        Label(screen,text="1 minute is the lowest you can go!",font=('Times', 26)).pack()
+    if l> 1:
+        Button(screen, image=downarrow,command=down,width=300,height=200, pady=10).pack()
+    Label(screen,text="Remember, this is measured in minutes!").pack()
     screen.mainloop()
+import tkinter
+from tkinter import *
+import time
+def countdown(t):
+    
+    while t:
+        
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    screen = tkinter.Tk()
+    screen.attributes('-fullscreen', True)
+    screen.title("Buttons, and more buttons!")
+    Label(screen,text="hi").pack()
+    Button(screen,text="start timer", command=countdown).pack()
+  
+  
+# input time in seconds
+t = l
+countdown(int(t))
 global all
 testscreen()
